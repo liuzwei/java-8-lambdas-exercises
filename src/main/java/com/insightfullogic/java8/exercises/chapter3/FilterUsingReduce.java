8a657849be0670ba8b6c4cacc1ab4da3f0e6e2bb
liuzhaowei <liuzhaowei1118>
2022-04-25 23:29:06 +0800

    练习

--- src/main/java/com/insightfullogic/java8/exercises/chapter3/FilterUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/FilterUsingReduce.java
@@ -4,6 +4,7 @@ import com.insightfullogic.java8.exercises.Exercises;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -13,7 +14,26 @@ import java.util.stream.Stream;
 public class FilterUsingReduce {
 
     public static <I> List<I> filter(Stream<I> stream, Predicate<I> predicate) {
-        return Exercises.replaceThisWithSolution();
+        return stream.reduce(new ArrayList<I>(), (List<I> acc, I t) -> {
+            List<I> newList = new ArrayList<>(acc);
+            if (predicate.test(t)){
+                newList.add(t);
+            }
+            return newList;
+        }, (List<I> left, List<I> right) -> {
+            List<I> newLeft = new ArrayList<>(left);
+            newLeft.addAll(right);
+            return newLeft;
+        });
     }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.forEach(consumerStrFunction);
+    }
+    static Consumer<String> consumerStrFunction = str -> {
+        System.out.println(str);
+    };
 
 }
--- src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
+++ src/main/java/com/insightfullogic/java8/exercises/chapter3/MapUsingReduce.java
@@ -5,6 +5,7 @@ import com.insightfullogic.java8.exercises.Exercises;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -13,7 +14,15 @@ import java.util.stream.Stream;
 public class MapUsingReduce {
 
     public static <I, O> List<O> map(Stream<I> stream, Function<I, O> mapper) {
-        return Exercises.replaceThisWithSolution();
+        return stream.reduce(new ArrayList<O>(), (List<O> acc, I x) -> {
+            ArrayList arrayList = new ArrayList<O>(acc);
+            arrayList.add(mapper.apply(x));
+            return arrayList;
+        }, (List<O> left, List<O> right) -> {
+            List<O> newLeft = new ArrayList<>(left);
+            newLeft.addAll(right);
+            return newLeft;
+        });
     }
 
 }
